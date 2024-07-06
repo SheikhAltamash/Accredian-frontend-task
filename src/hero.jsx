e7bca5df371b257db87f4f37f6e3af94abcd51c2
altamash <altamashsheikh077@gmail.com>
2024-07-06 20:56:56 +0530

    added pic

--- src/hero.jsx
+++ src/hero.jsx
@@ -7,14 +7,22 @@ export default function Hero({show, setshowform}) {
           </h1>
           <p className="hero_p_price">
             Get chance to win <br></br>up-to
-            <span> &nbsp;
-           Rs. 15,000
-            </span>
+            <span> &nbsp; Rs. 15,000</span>
           </p>
-          <button className="btn_nav hero_btn" onClick={()=>{setshowform(true)}}>Refer now</button>
+          <button
+            className="btn_nav hero_btn"
+            onClick={() => {
+              setshowform(true);
+            }}
+          >
+            Refer now
+          </button>
         </div>
         <div className="right">
-          <img src="/d.jpeg" alt="" />
+          <img
+            src="https://res.cloudinary.com/do4pj4m25/image/upload/v1720279479/samples/hpxwggesb3d3lnv18agu.jpg"
+            alt=""
+          />
         </div>
       </div>
     );
